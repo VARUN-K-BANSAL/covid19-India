9e4b7c83ffb6069f0653e5b3a1c619865a7fdee2
Varun Kumar Bansal <varunbansal326@gmail.com>
2021-10-13 14:34:25 +0530

    added district status page

--- javascript/districtReport.js
+++ javascript/districtReport.js
@@ -1,21 +1,35 @@
 const summaryAPI = "https://data.covid19india.org/state_district_wise.json";
+const stateAPI = "https://cdn-api.co-vin.in/api/v2/admin/location/states";
 
-$(document).ready(function() {
-    $.getJSON(summaryAPI, function(res) {
+$(document).ready(function () {
+    $.getJSON(stateAPI, function (res) {
         var i = 0;
-
-        console.log(res);
-        // var data = JSON.parse(res);
-        console.log(JSON.parse(res));
-        while(res[i] != undefined) {
-            var newRow = "<tr>" + 
-                            "<td>" + res['data'].regional[i].loc + "</td>" + 
-                            "<td>" + res['data'].regional[i].deaths + "</td>" + 
-                            "<td>" + res['data'].regional[i].totalConfirmed + "</td>" + 
-                        "</tr>";
-            
-            $("table").append(newRow);
+        console.log(res['states'][0]);
+        while (res['states'][i] != undefined) {
+            var newState = '<option value="' + res['states'][i].state_id + '" name="' + res['states'][i].state_name + '">' + res['states'][i].state_name + '</option>';
+            $('#state').append(newState);
             i++;
         }
+    });
+
+    $('#state').change(function () {
+        var stateName = $(this).children("option:selected").text();
+
+        $.getJSON(summaryAPI, function (data) {
+            // console.log(data);
+            // removing the rows except the first heading
+            $("table").find("tr:not(:first)").remove();
+
+            for (var districtName in data[stateName].districtData) {
+                var newRow = "<tr>" +
+                    "<td>" + districtName + "</td>" +
+                    "<td>" + data[stateName].districtData[districtName].recovered + "</td>" +
+                    "<td>" + data[stateName].districtData[districtName].confirmed + "</td>" +
+                    "<td>" + data[stateName].districtData[districtName].active + "</td>" +
+                    "</tr>";
+
+                $("table").append(newRow);
+            }
+        });
     });
 });
