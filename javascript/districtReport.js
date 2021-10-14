1de8aa2d4f08f55ffdfad0f4f4e94eaf01cd0030
Varun Kumar Bansal <varunbansal326@gmail.com>
2021-10-14 09:16:29 +0530

    Update

--- javascript/districtReport.js
+++ javascript/districtReport.js
@@ -1,21 +1,16 @@
 const summaryAPI = "https://data.covid19india.org/state_district_wise.json";
-const stateAPI = "https://cdn-api.co-vin.in/api/v2/admin/location/states";
 
 $(document).ready(function () {
-    $.getJSON(stateAPI, function (res) {
-        var i = 0;
-        console.log(res['states'][0]);
-        while (res['states'][i] != undefined) {
-            var newState = '<option value="' + res['states'][i].state_id + '" name="' + res['states'][i].state_name + '">' + res['states'][i].state_name + '</option>';
+    $.getJSON(summaryAPI, function (res) {
+        for(var state in res) {
+            var newState = '<option value="' + res[state].state_id + '" name="' + state + '">' + state + '</option>';
             $('#state').append(newState);
-            i++;
         }
     });
 
     $('#state').change(function () {
         var stateName = $(this).children("option:selected").text();
         $.getJSON(summaryAPI, function (data) {
-            // console.log(data);
             // removing the rows except the first heading
             $("table").find("tr:not(:first)").remove();
 
--- javascript/vaccineBooking.js
+++
@@ -1,3 +0,0 @@
-function generateOTP() {
-    $(".number").getText
-}
